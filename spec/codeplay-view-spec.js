38d277d644afebb0e62c09070e41f353edbe36b3
ananyakansal <ananyakansal99@gmail.com>
2020-11-11 02:32:20 -0500

    first commit, package folders created

---
+++ spec/codeplay-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CodeplayView from '../lib/codeplay-view';
+
+describe('CodeplayView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
